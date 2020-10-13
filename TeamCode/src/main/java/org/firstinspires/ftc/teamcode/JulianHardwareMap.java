caf4f952886144b699fe3483137b8f260ce86723
Cox24 <cox.julian@libertycommon.org>
2020-10-13 16:52:39 -0600

    hardware map attempt

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JulianHardwareMap.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class JulianHardwareMap {
+
+    public DcMotor Jasper      = null;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public JulianHardwareMap(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        Jasper  = hwMap.get(DcMotor.class, "tony");
+
+        Jasper.setDirection(DcMotor.Direction.FORWARD);
+
+        // Set all motors to zero power
+        Jasper.setPower(0);
+
+        // Not running with encoders
+        Jasper.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+    }
+}
