2195c2006654218a5aa03f51e4f5cce148a19035
Tony Ortiz <ortiz.antonio22@libertycommon.org>
2020-10-22 16:54:15 -0600

    trying to fix the wacky code issue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBaseTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBaseTeleop.java
@@ -43,9 +43,6 @@ public class DriveBaseTeleop extends LinearOpMode {
     private ElapsedTime     runtime = new ElapsedTime();
 
 
-    static final double     FORWARD_SPEED = 0.6;
-    static final double     TURN_SPEED    = 0.5;
-
     @Override
     public void runOpMode() {
 
