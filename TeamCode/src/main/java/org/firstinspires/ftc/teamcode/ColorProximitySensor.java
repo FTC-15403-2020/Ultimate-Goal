31129c7762c9bc10033c067ff7967ef4977be4a5
Cox24 <cox.julian@libertycommon.org>
2020-11-05 16:56:11 -0700

    working color sensor program???(maybe)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorProximitySensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorProximitySensor.java
@@ -28,14 +28,22 @@
  */
 
 package org.firstinspires.ftc.teamcode;
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 
 @TeleOp (name="ColorProximitySensor", group="hello")
@@ -54,8 +62,16 @@ public class ColorProximitySensor extends LinearOpMode {
     static double leftBackPower;
     static double rightFrontPower;
     static double rightBackPower;
+    static double color_sensor;
 
-    ColorSensor color_sensor;
+    protected void runSample() {
+        NormalizedRGBA colors = colorSensor.getNormalizedColors();
+        telemetry.addLine()
+                .addData("Red", "%.3f", colors.red)
+                .addData("Green", "%.3f", colors.green)
+                .addData("Blue", "%.3f", colors.blue);
+    }
+    NormalizedColorSensor colorSensor;
 
     @Override
     public void runOpMode() {
@@ -86,16 +102,14 @@ public class ColorProximitySensor extends LinearOpMode {
             telemetry.addData("Speed", robot.rightbackDrive.getPower());
             telemetry.update();
 
-            robot.color_sensor.red();
-            robot.color_sensor.green();
-            robot.color_sensor.blue();
+            //robot.color_sensor.red();
+            //robot.color_sensor.green();
+            //robot.color_sensor.blue();
 
-            robot.color_sensor.alpha();
-            robot.color_sensor.argb();
+            //robot.color_sensor.alpha();
+            //robot.color_sensor.argb();
 
-
-            telemetry.addData("Color", robot.color_sensor.red(), robot.color_sensor.green(), robot.color_sensor.blue());
+            runSample();
         }
-
     }
 }
