6a3987ccd185db87d1a3927fdd9bd9db6f0fced7
Riley-jpg <carlson.riley24@libertycommon.org>
2020-12-02 16:53:44 -0700

    Servos are horrible. Fix Wobble Grabber program tomorrow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WobbleGoalGrabber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WobbleGoalGrabber.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -11,21 +12,21 @@ public class WobbleGoalGrabber extends LinearOpMode {
     DcMotor rotationMotor;
     Servo grabberServo;
 
-
     public void runOpMode() throws InterruptedException {
         rotationMotor = hardwareMap.dcMotor.get("wobbleG");
         grabberServo = hardwareMap.servo.get("wobbleS");
+        grabberServo.setDirection(Servo.Direction.REVERSE);
 
-        final int SERVO_CLOSED  = 0;
+        final int SERVO_CLOSED  = 359;
         final int SERVO_OPEN    = 60;
-        int servoTargetPos = SERVO_CLOSED;
+
         double mPow = 0;
 
         waitForStart();
         while(opModeIsActive()) {
-            int servoPos = (int) grabberServo.getPosition();
+            //int servoPos = (int) grabberServo.getPosition();
             //mPow = gamepad1.left_trigger - gamepad1.right_trigger;
-            mPow = -gamepad1.left_stick_y/2;
+            mPow = gamepad1.left_stick_y/2;
             rotationMotor.setPower(mPow);
 
             /*if(gamepad1.left_bumper) {
